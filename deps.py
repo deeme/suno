6ec8905a97b3f371c1c019ac08f5c11d32a043a1
deeme <github.io@gmail.com>
2024-07-03 18:38:52 +0800

    更改提示

--- deps.py
+++ deps.py
@@ -1,7 +1,7 @@
-# -*- coding:utf-8 -*-
-
+# deps.py
 from cookie import get_next_available_auth
 from utils import get_credits
+import logging
 
 class AccountManager:
     def __init__(self):
@@ -11,14 +11,22 @@ class AccountManager:
     async def get_token(self):
         while True:
             token = self.current_auth.get_token()
-            credits = await get_credits(token)
-            if credits > 0:
-                return token
-            else:
+            try:
+                credits_info = await get_credits(token)
+                credits = credits_info.get('credits', 0)
+                logging.info(f"当前账号积分: {credits}")
+                
+                if credits > 0:
+                    return token
+                else:
+                    logging.warning(f"账号积分为0，尝试切换到下一个账号")
+                    self.current_auth = next(self.auth_generator)
+            except Exception as e:
+                logging.error(f"获取积分时出错: {str(e)}")
                 try:
                     self.current_auth = next(self.auth_generator)
                 except StopIteration:
-                    raise Exception("所有账号积分已用尽")
+                    raise Exception("所有账号积分已用尽或出现错误")
 
 account_manager = AccountManager()
 
