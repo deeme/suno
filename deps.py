0003e7dff9ef83d4b6fd40c26a0225a77480450b
deeme <github.io@gmail.com>
2024-07-03 21:35:29 +0800

    更改提示

--- deps.py
+++ deps.py
@@ -8,6 +8,8 @@ def get_token():
     while True:
         for suno_auth in suno_auths.values():
             token = suno_auth.get_token()
+            return token
+'''
             try:
                 credits_info = get_credits(token)
                 credits = credits_info.get('credits', 0)
@@ -17,6 +19,7 @@ def get_token():
                     return token
             except Exception as e:
                 logging.error(f"获取积分失败: {e}")
+'''
         
         logging.warning("所有账号积分已用尽,等待 1 小时后重试...")
         asyncio.sleep(3600)  # 等待1小时 (3600秒)
