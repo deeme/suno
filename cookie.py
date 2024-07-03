8db5e0f667295c81359c55c97fe5902acc6d703e
deeme <github.io@gmail.com>
2024-07-03 18:06:23 +0800

    更改提示

--- cookie.py
+++ cookie.py
@@ -16,6 +16,24 @@ class SunoCookie:
         self.session_id = None
         self.token = None
 
+    def load_cookie(self, cookie_str):
+        self.cookie.load(cookie_str)
+
+    def get_cookie(self):
+        return ";".join([f"{i}={self.cookie.get(i).value}" for i in self.cookie.keys()])
+
+    def set_session_id(self, session_id):
+        self.session_id = session_id
+
+    def get_session_id(self):
+        return self.session_id
+
+    def get_token(self):
+        return self.token
+
+    def set_token(self, token: str):
+        self.token = token
+
 def load_accounts():
     accounts = {}
     i = 1
@@ -31,22 +49,13 @@ def load_accounts():
         i += 1
     return accounts
 
-def keep_alive(suno_cookie: SunoCookie):
-    while True:
-        try:
-            update_token(suno_cookie)
-        except Exception as e:
-            print(e)
-        finally:
-            time.sleep(5)
-
 def update_token(suno_cookie: SunoCookie):
     headers = {"cookie": suno_cookie.get_cookie()}
     headers.update(COMMON_HEADERS)
     session_id = suno_cookie.get_session_id()
 
     resp = requests.post(
-        url=f"https://clerk.suno.com/v1/client/sessions/{session_id}/tokens?_clerk_js_version=4.73.3",
+        url=f"https://clerk.suno.com/v1/client/sessions/{session_id}/tokens?_clerk_js_version=4.72.0-snapshot.vc141245",
         headers=headers,
     )
 
@@ -55,6 +64,18 @@ def update_token(suno_cookie: SunoCookie):
     suno_cookie.load_cookie(set_cookie)
     token = resp.json().get("jwt")
     suno_cookie.set_token(token)
+    # print(set_cookie)
+    # print(f"*** token -> {token} ***")
+
+
+def keep_alive(suno_cookie: SunoCookie):
+    while True:
+        try:
+            update_token(suno_cookie)
+        except Exception as e:
+            print(e)
+        finally:
+            time.sleep(5)
 
 # 加载账号并启动保活
 accounts = load_accounts()
